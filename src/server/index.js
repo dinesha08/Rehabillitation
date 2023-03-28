cbd9c90da0307f27b0939c8c86d7eaee2e6c9bfd
P Mohana Prasad <p.mohanaprasad@outlook.com>
2023-03-28 14:38:12 +0530

    .

--- src/server/index.js
+++ src/server/index.js
@@ -20,9 +20,35 @@ let data = [];
 parser.on("data", (line) => data.push(line));
 
 app.get("/data", (req, res) => {
-  res.json(data);
+  res.send(data);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+
+  if (req.url === "/data") {
+    const data = { message: "Hello, React!" };
+    res.write(JSON.stringify(data));
+    res.end();
+  } else {
+    res.write("Invalid Request");
+    res.end();
+  }
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
