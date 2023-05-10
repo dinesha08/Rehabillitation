5a572090f5691e4b784d80cedc9d34af77b20938
P Mohana Prasad <p.mohanaprasad@outlook.com>
2023-05-10 16:51:03 +0530

    .

--- src/app/containers/Dashboard/profileSection.js
+++ src/app/containers/Dashboard/profileSection.js
@@ -1,10 +1,10 @@
-import { forEach, get } from "lodash";
-import React, { useRef } from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import ReactToPrint from "react-to-print";
-import { Col, Row, Card, CardBody } from "reactstrap";
+import { forEach, get } from 'lodash';
+import React, { useRef } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import ReactToPrint from 'react-to-print';
+import { Col, Row, Card, CardBody } from 'reactstrap';
 
 const ProfileSection = ({
   edit,
@@ -16,6 +16,8 @@ const ProfileSection = ({
   currentDate,
   setStageAcceleration,
   setStageBrake,
+  stageAcceleration,
+  stageBrake,
 }) => {
   const reportRef = useRef();
 
@@ -40,9 +42,9 @@ const ProfileSection = ({
       const minutes = Math.floor((difference % 3600000) / 60000);
       const seconds = Math.floor((difference % 60000) / 1000);
       setSessionTime(
-        `${hours.toString().padStart(2, "0")} hr ${minutes
+        `${hours.toString().padStart(2, '0')} hr ${minutes
           .toString()
-          .padStart(2, "0")} min ${seconds.toString().padStart(2, "0")} sec`
+          .padStart(2, '0')} min ${seconds.toString().padStart(2, '0')} sec`
       );
     } else {
       setSessionTime(0);
@@ -62,7 +64,7 @@ const ProfileSection = ({
           <Col className="mx-5 d-flex justify-content-center align-items-center">
             <Col xs="3" className="mx-4 my-2 text-center">
               <img
-                src={require("./assert/profilePic.png")}
+                src={require('./assert/profilePic.png')}
                 className="profilePic"
                 alt="profilePic"
               />
@@ -71,7 +73,7 @@ const ProfileSection = ({
               <Row>
                 <Col>
                   <h4>
-                    {personalDetails.firstName || "N/A"}{" "}
+                    {personalDetails.firstName || 'N/A'}{' '}
                     {personalDetails.lastName}
                   </h4>
                 </Col>
@@ -80,11 +82,11 @@ const ProfileSection = ({
                 <Col>
                   <h6>
                     <span className="key">Gender: </span>
-                    <span>{personalDetails.gender || "N/A"}</span>
+                    <span>{personalDetails.gender || 'N/A'}</span>
                   </h6>
                   <h6>
                     <span className="key">Age: </span>
-                    <span>{personalDetails.age || "N/A"}</span>
+                    <span>{personalDetails.age || 'N/A'}</span>
                   </h6>
                 </Col>
                 <Col className="mx-4">
@@ -92,27 +94,27 @@ const ProfileSection = ({
                     <span className="key">Height: </span>
                     <span>
                       {healthDetails.height
-                        ? healthDetails.height + " ft"
-                        : "N/A"}
+                        ? healthDetails.height + ' ft'
+                        : 'N/A'}
                     </span>
                   </h6>
                   <h6>
                     <span className="key">Weight: </span>
                     <span>
                       {healthDetails.weight
-                        ? healthDetails.weight + " kg"
-                        : "N/A"}
+                        ? healthDetails.weight + ' kg'
+                        : 'N/A'}
                     </span>
                   </h6>
                 </Col>
                 <Col xs="6" className="mx-4">
                   <h6>
                     <span className="key">Region of Injury: </span>
-                    <span>{healthDetails.regionOfInjury || "N/A"}</span>
+                    <span>{healthDetails.regionOfInjury || 'N/A'}</span>
                   </h6>
                   <h6>
                     <span className="key">Cause of Injury: </span>
-                    <span>{healthDetails.causeOfInjury || "N/A"}</span>
+                    <span>{healthDetails.causeOfInjury || 'N/A'}</span>
                   </h6>
                 </Col>
               </Row>
@@ -132,13 +134,13 @@ const ProfileSection = ({
               <Col>
                 <span className="reportSubTitle">Date: </span>
                 <span className="reportWord">
-                  {get(patientDetails, "generalInfo.currentDate", currentDate)}
+                  {get(patientDetails, 'generalInfo.currentDate', currentDate)}
                 </span>
               </Col>
               <Col className="end-0">
                 <span className="reportSubTitle">Session Timing: </span>
                 <span className="reportWord">
-                  {get(patientDetails, "generalInfo.sessionTime", sessionTime)}
+                  {get(patientDetails, 'generalInfo.sessionTime', sessionTime)}
                 </span>
               </Col>
             </Row>
@@ -150,8 +152,8 @@ const ProfileSection = ({
             <Row>
               <Col xs="4" className="d-flex flex-column">
                 <span className="reportWord">
-                  Name:{" "}
-                  {personalDetails.firstName + " " + personalDetails.lastName}
+                  Name:{' '}
+                  {personalDetails.firstName + ' ' + personalDetails.lastName}
                 </span>
               </Col>
               <Col xs="4" className="d-flex flex-column">
@@ -224,7 +226,7 @@ const ProfileSection = ({
             <Row>
               <Col xs="4" className="d-flex flex-column">
                 <span className="reportWord">
-                  Name: {kinDetails.firstName + " " + kinDetails.lastName}
+                  Name: {kinDetails.firstName + ' ' + kinDetails.lastName}
                 </span>
               </Col>
               <Col xs="4" className="d-flex flex-column">
@@ -256,8 +258,22 @@ const ProfileSection = ({
             <Row className="mb-3 mt-3">
               <Col>
                 <span className="reportSubTitle">Stages Reached: </span>
-                <span className="reportWord">Acceleration - {forEach}</span>
-                <span className="reportWord">Brake -</span>
+                <span className="reportWord">
+                  Acceleration -{' '}
+                  {forEach(stageAcceleration, (value, key) => {
+                    if (value) {
+                      return '*';
+                    }
+                  })}
+                </span>
+                <span className="reportWord">
+                  Brake -{' '}
+                  {forEach(stageBrake, (value, key) => {
+                    if (value) {
+                      return '*';
+                    }
+                  })}
+                </span>
               </Col>
             </Row>
           </div>
